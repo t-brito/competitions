ca49be838dd3680d01633f2e1b272b3010c74909
Tiago Brito <tiago.v.brito@gmail.com>
2018-03-14 22:14:18 +0000

    added entire nodeschool.io javascripting module

---
+++ nodeschool.io/javascripting/09-if-statement.js
@@ -0,0 +1,11 @@
+var fruit = 'orange';
+var string;
+
+if (fruit.length > 5) {
+  string = 'more than five characters.';
+}
+else {
+  string = 'five characters or less.';
+}
+
+console.log('The fruit name has ' + string);
